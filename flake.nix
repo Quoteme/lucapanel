1fb445118dd10d18e665e772a0e7c9171c7c5b1e
Luca Leon Happel <luca.happel@hhu.de>
2024-08-04 09:44:01 +0200

    add: initial working version

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "A simple panel for the xmonad-luca desktop experience";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        flutterDependencies = with pkgs; [
+          flutter
+          pcre
+          fontconfig
+          # Dependencies for flutter
+          at-spi2-core.dev
+          clang
+          cmake
+          dart
+          dbus.dev
+          # flutter
+          gtk3
+          libdatrie
+          libepoxy
+          libselinux
+          libsepol
+          libthai
+          libxkbcommon
+          ninja
+          pcre
+          pkg-config
+          util-linux.dev
+          xorg.libXdmcp
+          xorg.libXtst
+          cairo.dev
+        ];
+      in
+      rec {
+        defaultPackage = packages.lucapanel;
+        packages.lucapanel = with pkgs; stdenv.mkDerivation (finalAttrs: {
+          pname = "lucapanel";
+          version = "1.0";
+
+          src = ./.;
+
+          nativeBuildInputs = [
+            makeWrapper
+          ];
+          buildInputs = [
+            flutter
+          ];
+        });
+
+        devShells.default = with pkgs; mkShell {
+          NIX_LD_LIBRARY_PATH = lib.makeLibraryPath ([
+            stdenv.cc.cc
+            openssl
+          ] ++ flutterDependencies);
+          NIX_LD = runCommand "ld.so" { } ''
+            ln -s "$(cat '${pkgs.stdenv.cc}/nix-support/dynamic-linker')" $out
+          '';
+          buildInputs = flutterDependencies;
+        };
+      }
+    );
+}
---
+++ panel/android/app/src/main/kotlin/com/example/lucapanel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lucapanel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
